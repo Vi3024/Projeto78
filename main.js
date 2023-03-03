07b58e15475d16c50f53c0a188dcb8bbc50c4e2c
Vi3024 <122474961+Vi3024@users.noreply.github.com>
2023-03-03 14:05:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var images =
-    ["https://i.postimg.cc/MKdhy06Z/family.jpg", "https://o.remove.bg/downloads/202f4024-def3-40c6-aca0-0341c0fe5453/fdac4b53fd416f318c75a8c73bd768ec-removebg-preview.png",
+    ["https://i.postimg.cc/MKdhy06Z/family.jpg", "https://img.freepik.com/vetores-premium/homens-polegares-para-cima-ilustracao-de-personagem-de-desenho-animado_52453-39.jpg",
         "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg",
         "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
 
